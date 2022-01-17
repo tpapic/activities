2a7f8da547abc5e5f16d63290988d7255fe815f2
tomislav.papic <tomislavpapic7@gmail.com>
2022-01-17 16:28:03 +0100

    Add react client

---
+++ client-app/src/App.tsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import axios from 'axios'
+import { Header, List } from 'semantic-ui-react';
+
+function App() {
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        // axios.get('http://localhost:5000/api/activities').then(res => {
+        //     console.log(res);
+        //     setActivities(res.data);
+        // })
+    });
+
+  return (
+    <div>
+        <Header as='h2' icon='users' content='Activities'/>
+        <List>
+            {activities.map((a: any) => (
+                <List.Item key={a.id}> {a.title} </List.Item>
+            ))}
+        </List>
+    </div>
+  );
+}
+
+export default App;
